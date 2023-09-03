460d0ee695fb57fb91114ac208584090cde1bf11
abhinav69217 <abhinav0045.be21@chitkara.edu.in>
2023-09-03 17:13:06 +0530

    linked list

---
+++ linkedList/insertAtHead.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int data)
+{
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    insertAtHead(head, 20);
+
+    print(head);
+
+    return 0;
+}
